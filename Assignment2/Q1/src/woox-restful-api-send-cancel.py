45819dd2b0e27c7e291a2b3e6c1d5629e7e17976
hsiuyee <hsiuyee.sc11@nycu.edu.tw>
2024-11-09 18:56:04 +0800

    modify woox-restful-api-send-cancel, add send_order,  edit_order_by_client_order_id, cancel_order_by_client_order_id and cancel_all_pending_orders API functions

---
+++ Assignment2/Q1/src/woox-restful-api-send-cancel.py
@@ -0,0 +1,145 @@
+import datetime
+import hmac, hashlib, base64
+import requests
+import json
+
+class WooXStagingAPI:
+    def __init__(self, api_key, api_secret, base_url='https://api.staging.woo.org'):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.base_url = base_url
+
+    # https://docs.woox.io/#authentication
+    def make_request(self, endpoint, params, requests_type="get", version="v1"):
+        # https://docs.woox.io/#authentication
+        def _generate_signature(params):
+            key = self.api_secret
+            key_bytes= bytes(key, 'utf-8')
+            params_bytes = bytes(params, 'utf-8')
+            return hmac.new(key_bytes, params_bytes, hashlib.sha256).hexdigest()
+        
+        milliseconds_since_epoch = round(datetime.datetime.now().timestamp() * 1000)
+        url = self.base_url + endpoint
+        print("\nurl", url, "\n")
+
+        # timestamp, http request method, request_path and request_body
+        if version == "v1":
+            body = "&".join(f"{key}={value}" for key, value in params.items())
+            body = body + "|"+str(milliseconds_since_epoch)
+        else:
+            body = json.dumps(params)
+            body = f'{milliseconds_since_epoch}{requests_type.upper()}{endpoint}{body}'
+
+        # Set headers
+        headers = {
+            'x-api-timestamp': str(milliseconds_since_epoch),
+            'x-api-key': self.api_key,
+            'x-api-signature': _generate_signature(body),
+            'Content-Type': 'application/json' if version == "v3" else 'application/x-www-form-urlencoded',
+            'Cache-Control': 'no-cache'
+        }
+        print("body", body)
+
+        if requests_type == "get":
+            if version == "v1":
+                response = requests.get(url, params=params)
+            response.raise_for_status()
+        elif requests_type == "post":
+            if version == "v1":
+                response = requests.post(url, headers=headers, params=params)
+        elif requests_type == "put":
+            if version == "v1":
+                response = requests.put(url, headers=headers, params=params)
+            else:
+                response = requests.put(url, headers=headers, json=params)
+        elif requests_type == "delete":
+            if version == "v1":
+                response = requests.delete(url, headers=headers, params=params)
+            else:
+                response = requests.delete(url, headers=headers, json=params)
+        return response.json()
+        
+    
+    # https://docs.woox.io/#orderbook-snapshot-public
+    def get_orderbook(self, symbol, max_level=100):
+        endpoint = f'/v1/public/orderbook/{symbol}'
+        params = {
+            'max_level': max_level,
+        }
+        return self.make_request(endpoint, params)
+        # pass
+
+    # https://docs.woo.org/#market-trades-public
+    def get_trades(self, symbol, limit=100):
+        endpoint = f'/v1/public/market_trades'
+        params = {
+            'symbol': symbol,
+            'limit': limit,
+        }
+        return self.make_request(endpoint, params)
+        # pass
+
+    # https://docs.woo.org/#kline-public
+    def get_kline(self, symbol, interval="1m", limit=100):
+        endpoint = f'/v1/public/kline'
+        params = {
+            'symbol': symbol,
+            "type": interval,
+            'limit': limit,
+        }
+        return self.make_request(endpoint, params)
+    
+    # https://docs.woox.io/?python#send-order
+    def post_send_order(self, params):
+        endpoint = '/v1/order'
+        return self.make_request(endpoint, params, "post")
+
+    def edit_order_by_client_order_id(self, params, client_order_id):
+        endpoint = f'/v3/order/client/{client_order_id}'
+        return self.make_request(endpoint, params, requests_type="put", version="v3")
+
+    def cancel_order_by_client_order_id(self, params):
+        endpoint = '/v1/client/order'
+        print("params", params)
+        return self.make_request(endpoint, params, requests_type="delete", version="v1")
+    
+    def cancel_all_pending_orders(self):
+        endpoint = '/v3/orders/pending'
+        return self.make_request(endpoint, params={}, requests_type="delete", version="v3")
+
+if __name__ == "__main__":
+    api_key = 'sdFgbf5mnyDD/wahfC58Kw=='
+    api_secret = 'FWQGXZCW4P3V4D4EN4EIBL6KLTDA'
+    woox_api = WooXStagingAPI(api_key, api_secret)
+    
+    # test post_send_order
+    params = {
+        'client_order_id': 2,
+        'order_price': 0.21,
+        'order_quantity': 10,
+        'order_type': 'LIMIT',
+        'side':'BUY',
+        'symbol': 'SPOT_BTC_USDT'
+    }
+    response = woox_api.post_send_order(params)
+    print(response)
+
+    # # test edit_order_by_client_order_id
+    # params = {
+    #     "client_order_id": "1",
+    #     "quantity": "11"
+    # }
+    # response = woox_api.edit_order_by_client_order_id(params, 1)
+    # print(response)
+
+    # # test cancel_all_pending_orders
+    # response = woox_api.cancel_all_pending_orders()
+    # print(response)
+
+    # test cancel_order_by_client_order_id
+    # params = {
+    #     "client_order_id": 2,
+    #     'symbol': 'SPOT_BTC_USDT'
+    # }
+    # response = woox_api.cancel_order_by_client_order_id(params)
+    # print(response)
