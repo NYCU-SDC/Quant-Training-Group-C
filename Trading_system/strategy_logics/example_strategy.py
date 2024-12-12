05dd6540428f8927b8bb87cc692df9bccc5b6cd2
jameschiu5487 <jameschiu5487@gmail.com>
2024-12-12 19:06:32 +0800

    update strategy framework

--- Trading_system/strategy_logics/example_strategy.py
+++ Trading_system/strategy_logics/example_strategy.py
@@ -7,23 +7,39 @@ import hmac
 import hashlib
 import aioredis  # Redis client for async operations
 from strategy_logics.strategy_init import Strategy
+import logging
 
 class ExampleStrategy(Strategy):
     """Example: send order each 1 sec."""
-    async def execute(self, channel, data, redis_client):
-        # Simulate signal generation
-        await asyncio.sleep(0.1)
-        print(f"[ExampleStrategy] Processing data from {channel}: {data}")
+    def __init__(self, signal_channel):
+        self.signal_channel = signal_channel
+        self.strategy_name = "ExampleStrategy"
+        self.order_id = []
+        self.ask_limit_order = dict()
+        self.bid_limit_order = dict()
+        self.number = 0
+        # General logger
+        self.logger = self.setup_logger(self.strategy_name)
+
+        # Configure logger for filled orders
+        self.fill_order_logger = self.setup_logger(
+            f"{self.strategy_name}_fills", f"{self.strategy_name}_fill_orders.log"
+        )
+    async def process_market_data(self, channel, data, redis_client):
+        """Handle market data (e.g., price updates)."""
+        self.logger.info(f"[{self.strategy_name}] Processing market data from {channel}: {data}")
 
         # Example signal generation
-        signal = {
-            "target": "send_order",
-            "order_price": 90000,
-            "order_quantity": 0.0001,
-            "order_type": "LIMIT",
-            "side": "BUY",
-            "symbol": "SPOT_BTC_USDT"
-        }
+        signal = self.create_order_signal(
+            price=96900,
+            quantity=0.0001,
+            side="BUY",
+            symbol='SPOT_BTC_USDT',
+        )
 
-        # Publish the signal to the order executor
+        self.order_id.append(self.number)
+        self.number += 1
+
         await self.publish_signal(signal, redis_client)
+                
+            
